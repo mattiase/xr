6587fa4b2fcb6d91c48ab56a95c04c7d2e514517
Mattias Engdegård <mattias.engdegard@gmail.com>
2025-07-27 13:15:42 +0200

    Update email

--- xr-test.el
+++ xr-test.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2019-2020 Free Software Foundation, Inc.
 
-;; Author: Mattias Engdegård <mattias.engdgard@gmail.com>
+;; Author: Mattias Engdegård <mattias.engdegard@gmail.com>
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
